93f22b444e8c67cdce1089cf6c5588be526ac8b1
Amir Barylko <amir@barylko.com>
2011-05-31 16:59:14 -0500

    Added license file

--- main/GlobalAssemblyInfo.cs
+++ main/GlobalAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("MavenThought Inc.")]
-[assembly: AssemblyProduct("MavenThought Commons (sha 340552efc55)")]
+[assembly: AssemblyProduct("MavenThought Commons (sha 4f06a17ccd2)")]
 [assembly: AssemblyCopyright("MavenThought Inc. 2006 - 2011")]
 [assembly: AssemblyVersion("0.9.4")]
 
